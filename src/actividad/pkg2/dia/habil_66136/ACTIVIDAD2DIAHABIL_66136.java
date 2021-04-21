91bd22ce65b9be0ca436ef7106ca7e069edf299b
salva <salva@LAPTOP-B2KFIIUB>
2021-04-21 18:24:30 -0500

    Actividad 1 if_66136

--- src/actividad/pkg2/dia/habil_66136/ACTIVIDAD2DIAHABIL_66136.java
+++ src/actividad/pkg2/dia/habil_66136/ACTIVIDAD2DIAHABIL_66136.java
@@ -5,6 +5,8 @@
  */
 package actividad.pkg2.dia.habil_66136;
 
+import java.util.Scanner;
+
 /**
  *
  * @author salva
@@ -16,6 +18,40 @@ public class ACTIVIDAD2DIAHABIL_66136 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    String text = null;
+    String text2 = null; 
     
+    Scanner sc = new Scanner(System.in);
+        System.out.println("(1) Lunes" + "\n(2) Martes" + "\n(3) Miércoles" + "\n(4) Jueves" + "\n(5) Viernes" + "\n(6) Sábado" + "\n(7) Domingo");
+    int dia = sc.nextInt();
+                     
+if (dia >= 1 && dia <=7) {
+            switch (dia) {
+                case 1:
+                    System.out.println("Lunes es un día hábil.");
+                    break;
+                case 2:
+                    System.out.println("Martes es un día hábil.");
+                    break;
+                case 3:
+                    System.out.println("Miércoles es un día hábil.");
+                    break;
+                case 4:
+                    System.out.println("Jueves es un día hábil.");
+                    break;
+                case 5:
+                    System.out.println("Viernes es un día hábil.");
+                    break;
+                case 6:
+                    System.out.println("Sábado es fin de semana.");
+                    break;
+                case 7:
+                    System.out.println("Domingo es fin de semana.");
+                    break;
+            }   
+            
+        } else {
+            System.out.println("Que dia desea selleccionar?.");
+        } 
+    }
 }
---
+++ test/actividad/pkg2/dia/habil_66136/ACTIVIDAD2DIAHABIL_66136NGTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad.pkg2.dia.habil_66136;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author salva
+ */
+public class ACTIVIDAD2DIAHABIL_66136NGTest {
+    
+    public ACTIVIDAD2DIAHABIL_66136NGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of main method, of class ACTIVIDAD2DIAHABIL_66136.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ACTIVIDAD2DIAHABIL_66136.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
